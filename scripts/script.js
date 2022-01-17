81c00717e4912fc2badb9dc600420bff8c86f2a2
Jesse Pantoja <pantojabruin@gmail.com>
2022-01-16 22:04:59 -0800

    Add flex properties and some debugging visual styling

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,11 @@ function createGrid(){
 
     for (let i = 0; i < 16; i++){
         let row = document.createElement('div');
+        row.classList.add('row');
         for (let j = 0; j < 16; j++){
             let column = document.createElement('div');
+        column.classList.add('column');
+
             row.appendChild(column);
         };
         gridContainer.appendChild(row);

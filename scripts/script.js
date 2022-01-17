4a0c05fded1e3dc642ce0375eaeb8992e4ca8aa5
Jesse Pantoja <pantojabruin@gmail.com>
2022-01-16 22:28:56 -0800

    Implement color on hover over effect

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,26 @@
-function createGrid(){
-    const gridContainer = document.querySelector('.grid-container')
+const gridContainer = document.querySelector('.grid-container')
 
+function createGrid() {
     for (let i = 0; i < 16; i++){
         let row = document.createElement('div');
         row.classList.add('row');
         for (let j = 0; j < 16; j++){
             let column = document.createElement('div');
-        column.classList.add('column');
-
+            column.classList.add('column');
             row.appendChild(column);
         };
         gridContainer.appendChild(row);
     };
 };
 
-createGrid()
+function colorSquare(event) {
+    const box = event.target
+    if (!box.classList.contains('column')) return
+    box.classList.add('colored')
+}
+
+createGrid();
+gridContainer.addEventListener('mouseover', colorSquare)
+
+
+

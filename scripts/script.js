99cb02167e18c9373fd2d5f59ba967bc217d67ca
Jesse Pantoja <pantojabruin@gmail.com>
2022-01-16 23:11:08 -0800

    Implement core etch a sketch game

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 const gridContainer = document.querySelector('.grid-container')
+const resetGridButton = document.querySelector('#reset-grid-btn')
 
-function createGrid() {
-    for (let i = 0; i < 16; i++){
+function createGrid(sideLength) {
+    for (let i = 0; i < sideLength; i++){
         let row = document.createElement('div');
         row.classList.add('row');
-        for (let j = 0; j < 16; j++){
+        for (let j = 0; j < sideLength; j++){
             let column = document.createElement('div');
             column.classList.add('column');
             row.appendChild(column);
@@ -19,8 +20,22 @@ function colorSquare(event) {
     box.classList.add('colored')
 }
 
-createGrid();
+function resetGrid(){
+    gridContainer.innerHTML = '';
+    let userInput;
+    while(true) {
+        userInput = Number(prompt('How big do you want your grid? Enter a number x to build an x by x grid.'));
+        if (!(userInput > 100 || userInput < 1 || userInput === NaN)) break;
+        alert('Sorry! You must enter a number greater than 0 and less than 101. Try again.');
+    }
+
+    createGrid(userInput);
+}
+
+createGrid(16);
 gridContainer.addEventListener('mouseover', colorSquare)
+resetGridButton.addEventListener('click', resetGrid)
+
 
 
 
